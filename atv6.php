d3c4824b709c4be4710d89107ccaacded0c19651
Diogo <diogopbatista9412@gmail.com>
2023-04-24 21:40:26 -0300

    mais um finalizado

---
+++ atv6.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title></title>
+</head>
+<body>
+    <form action="atv6acao.php" method="post">
+        <label for="hora">Digite a hora (formato 24 horas):</label>
+        <input type="text" id="hora" name="hora">
+        <input type="submit" value="Converter">
+    </form>
+</body>
+</html>
